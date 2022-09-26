3577ee9d94e0d95b72824e7ad0146d92660eb9e8
nickgric <nick.gric@gmail.com>
2022-09-26 17:31:05 +0300

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryContentArr = [];
+
+galleryItems.forEach((galleryItem) => {
+  let galleryContentElement = `<div class="gallery__item"><a class="gallery__link" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"/></a></div>`;
+  galleryContentArr.push(galleryContentElement);
+});
+
+const galleryContentStr = galleryContentArr.join("");
+gallery.insertAdjacentHTML("afterbegin", galleryContentStr);
+
+const openModal = (event) => {
+  event.preventDefault();
+  if (event.target === gallery) {
+    return;
+  }
+  const closeModalEsc = (event) => {
+    if (event.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  };
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: () => window.addEventListener("keydown", closeModalEsc),
+      onClose: () => window.removeEventListener("keydown", closeModalEsc),
+    }
+  );
+
+  instance.show();
+};
+
+gallery.addEventListener("click", openModal);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryContentArr = [];
+
+galleryItems.forEach((galleryItem) => {
+  let galleryContentElement = `<a class="gallery__item" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" title="${galleryItem.description}" alt="${galleryItem.description}" /></a>`;
+  galleryContentArr.push(galleryContentElement);
+});
+
+const galleryContentStr = galleryContentArr.join("");
+gallery.insertAdjacentHTML("afterbegin", galleryContentStr);
+
+const openModal = (event) => {
+  event.preventDefault();
+  if (event.target === gallery) {
+    return;
+  }
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+  });
+};
+
+gallery.addEventListener("click", openModal);
